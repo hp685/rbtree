435e66cc3456ae35d6a64209f40da50cbc26ea24
hp685 <harsh@nyu.edu>
2019-03-15 23:18:02 -0700

    change comparison to string equals and add ordering test

--- test_rbtree.c
+++ test_rbtree.c
@@ -4,7 +4,7 @@
 
 void test_rbtree_alloc(){
 	struct rb_tree *tree = rb_tree_alloc();
-	TEST_ASSERT_EQUAL(tree->root->key, "NIL");
+	TEST_ASSERT_EQUAL_STRING(tree->root->key, "NIL");
 	TEST_ASSERT_EQUAL(tree->root->color, 0);
 	TEST_ASSERT_EQUAL(tree->root->parent, NULL);
 }
@@ -31,12 +31,32 @@ void test_insert_and_retrieve(){
 }
 
 
+void test_check_ordering(){
+	struct rb_tree *tree = rb_tree_alloc();
+	struct rb_node *node;
+	char s[2];
+	s[1] = '\0';
+
+	for(char c = 'a'; c <= 'z'; ++c){
+		s[0] = c;
+		node = rb_node_alloc_kv(s, s);
+		rb_insert(tree, node);
+	}
+	s[0] = 'a';
+	for(char c = 'b'; c <= 'z'; ++c){
+		node = rb_search(tree, s);
+		node = tree_successor(node);
+		s[0] = c;
+		TEST_ASSERT_EQUAL_STRING(node->key, s);
+	}
+}
 
 int main(int argc, char const *argv[])
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_rbtree_alloc);
 	RUN_TEST(test_insert_and_retrieve);
+	RUN_TEST(test_check_ordering);
 	UNITY_END();
 
 	return 0;
